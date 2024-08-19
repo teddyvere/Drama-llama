22478e02eac7e76ca38b0a3917e36cef0cff3d46
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-08-19 10:46:35 -0400

    port updated

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import os
 from website import create_app
 
 app = create_app()
 with app.app_context():
     # Your code here that needs to run within the app context
     pass # To avoid indentation error
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
