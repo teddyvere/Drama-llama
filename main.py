ae9cbcf12a1808908f64d936c99d796fe70562b9
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-08-20 10:50:22 -0400

    chat text output style revamed

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from website import create_app, db
 
 app = create_app()
 with app.app_context():
-    db.create_all()  # Create the database tables
-    # Your code here that needs to run within the app context
-    pass # To avoid indentation error
+    db.create_all()
+    pass 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port,debug=True)
