679acc0b9cf2865dde8c77d7dbb7edc9f104c996
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-08-23 11:28:10 -0400

    v52

--- main.py
+++ main.py
@@ -11,38 +11,7 @@ app = create_app()
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
-import traceback
 
-with app.app_context():
-    traceback.print_stack()
-    try:
-        # Check the add user function
-        logging.info("with app.app_context():")
-
-        logging.info(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
-
-        # Check database connection
-        db.session.execute(text('SELECT 1'))
-        logging.info("Database connection successful")
-    except Exception as e:
-        logging.error("Database connection failed: %s", e)
-    
-    # Inspect the database for the presence of tables
-    try:
-        inspector = inspect(db.engine)
-        table_models = {
-            'users': Users,
-            'prompt': Prompt,
-            'poem': Poem
-        }
-        
-        # Create tables if they do not exist
-        for table_name, model_class in table_models.items():
-            if not inspector.has_table(table_name):
-                model_class.__table__.create(bind=db.engine)
-                logging.info(f"Created table {table_name}")
-    except Exception as e:
-        logging.error(f"Error inspecting or creating tables: {e}")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,8 @@
+import inspect
+import logging
+import traceback
 import dotenv
-from flask import Flask
+from flask import Flask, app, text
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -7,6 +10,8 @@ from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
 import webbrowser
+
+from website.models import Poem, Prompt, Users
 
 load_dotenv()
 class Config:
@@ -127,6 +132,39 @@ def create_app():
     
     app.register_blueprint(routes, url_prefix='/')  # Register Blueprints
     app.register_blueprint(views, url_prefix='/')  # Register Blueprints
+    
+    import traceback
+
+    with app.app_context():
+        traceback.print_stack()
+        try:
+            # Check the add user function
+            logging.info("with app.app_context():")
+
+            logging.info(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
+
+            # Check database connection
+            db.session.execute(text('SELECT 1'))
+            logging.info("Database connection successful")
+        except Exception as e:
+            logging.error("Database connection failed: %s", e)
+        
+        # Inspect the database for the presence of tables
+        try:
+            inspector = inspect(db.engine)
+            table_models = {
+                'users': Users,
+                'prompt': Prompt,
+                'poem': Poem
+            }
+            
+            # Create tables if they do not exist
+            for table_name, model_class in table_models.items():
+                if not inspector.has_table(table_name):
+                    model_class.__table__.create(bind=db.engine)
+                    logging.info(f"Created table {table_name}")
+        except Exception as e:
+            logging.error(f"Error inspecting or creating tables: {e}")
     
 
     return app
