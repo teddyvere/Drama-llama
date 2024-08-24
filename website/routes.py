6dff006fe1d8b71e710aea27a60b7bdfa9887daa
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-08-24 16:24:48 -0400

    minor fix

--- website/routes.py
+++ website/routes.py
@@ -55,7 +55,7 @@ def sign_up():
             flash("Password must be at least 7 characters long.", category='error')
         else:
             try:
-                new_user = Users(email=email, first_name=first_name, password=generate_password_hash(password1, 'sha256'))
+                new_user = Users(email=email, first_name=first_name, password=generate_password_hash(password1, 'pbkdf2:sha256'))
                 db.session.add(new_user)
                 db.session.commit()
                 login_user(new_user, remember=True)
